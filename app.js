a3bef6a32079d4a043fc3ccdc94b084df614174f
Tis4tom <tdmorgan_mac@icloud.com>
2022-08-09 11:45:26 -0400

    format js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const slides = document.querySelectorAll('.nf-slide-wrapper img');
 const tabGalleryWrapper = document.getElementById('tab-gallery-wrapper');
 tabGalleryWrapper.innerHTML = `<img id="tab-gallery" src=\"${slides[0].src}\" />`;
-tabGallery = document.getElementById('tab-gallery');
-figCap = document.getElementById('fig-cap');
+const tabGallery = document.getElementById('tab-gallery');
+const figCap = document.getElementById('fig-cap');
 figCap.innerHTML = `<figcaption>${slides[0].alt}</figcaption>`;
  
 for (let slide of slides) {
